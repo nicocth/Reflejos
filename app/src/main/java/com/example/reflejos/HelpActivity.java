1a810bf1dee5aff446747faaf0dd26c5c11a9b77
nicocth <115360696+nicocth@users.noreply.github.com>
2024-05-01 02:39:02 +0200

    actividad Main y todas las que derivan de ella creadas (#14)
    * Integrando Login con Firebase Authentication
    
    * Login integrado con Firebase Authentification y MainActivity creada que contiene cabecera funcional con información de usuario y boton de cerrar sesión y cuerpo de la actividad con botones creados sin funcionalidad.
    
    * formulario de registrar usuario creado, completamente funcional
    
    * boton Volver atrás añadido en formulario para registrar usuario
    
    * Errores en formulario corregidos
    
    * Firestore integrado (#11)
    
    * Errores en formulario corregidos
    
    * Formulario con firestore implantado y mainactivity con iniciacion cabecera modulado
    
    * Actividades derivadas de Main implantadas (#13)
    
    * Errores en formulario corregidos
    
    * Formulario con firestore implantado y mainactivity con iniciacion cabecera modulado
    
    * Botones de MainActivity llevan a sus Activities correspondientes (#12)
    
    Co-authored-by: anngelicaa <angelicavmp@gmail.com>
    
    * Cabecera modulada e implantada en todas las actividades
    
    ---------
    
    Co-authored-by: anngelicaa <angelicavmp@gmail.com>
    
    ---------
    
    Co-authored-by: anngelicaa <angelicavmp@gmail.com>

--- app/src/main/java/com/example/reflejos/HelpActivity.java
+++ app/src/main/java/com/example/reflejos/HelpActivity.java
@@ -1,51 +1,38 @@
 package com.example.reflejos;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
 import org.checkerframework.checker.nullness.qual.NonNull;
+
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
-    //declaración de los elementos de la vista
-    private Button trainingButton, recordButton, createTrainingButton;
-    private Button devicesButton, helpButton;
+public class HelpActivity extends AppCompatActivity {
 
     //declaración del módulo Authentification de firebase
     private FirebaseAuth mAuth;
 
     //declaración del módulo Firestore
     private FirebaseFirestore db;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //inicializamos cabecera
+        setContentView(R.layout.activity_help);
         inicializarCabecera();
-
-        //inicializamos elementos del cuerpo de la vista
-        trainingButton = findViewById(R.id.trainingButton);
-        recordButton = findViewById(R.id.recordButton);
-        createTrainingButton = findViewById(R.id.createTrainingButton);
-        devicesButton = findViewById(R.id.devicesButton);
-        helpButton = findViewById(R.id.helpButton);
-
-        //Configuramos los botones
-        configurarBotones();
     }
-
 
     private void inicializarCabecera() {
         //inicializamos clase de autentificacion firebase
@@ -61,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
 
         //Obtenemos email del usuario
         String emailUser = Objects.requireNonNull(mAuth.getCurrentUser()).getEmail();
-
         //Configuramos informacion de usuario
         emailTextView.setText(emailUser);
 
@@ -94,49 +80,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mAuth.signOut();
                 finish();
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            }
-        });
-    }
-
-    private void configurarBotones() {
-        //Configuración de botón Entrenamientos
-        trainingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, TrainingActivity.class));
-            }
-        });
-
-        //Configuración de botón Historial
-        recordButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, RecordActivity.class));
-            }
-        });
-
-        //Configuración de botón Crear Entrenamiento
-        createTrainingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CreateTrainingActivity.class));
-            }
-        });
-
-        //Configuración de botón Dispositivos
-        devicesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, DevicesActivity.class));
-            }
-        });
-
-        //Configuración de botón Ayuda
-        helpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, HelpActivity.class));
+                startActivity(new Intent(HelpActivity.this, LoginActivity.class));
             }
         });
     }
--- app/src/main/java/com/example/reflejos/LoginActivity.java
+++ app/src/main/java/com/example/reflejos/LoginActivity.java
@@ -1,16 +1,13 @@
 package com.example.reflejos;
 
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
@@ -41,6 +38,7 @@ public class LoginActivity extends AppCompatActivity {
 
         //Configuracion de la Autentificacion firebase
         setup();
+
     }
 
     /**
